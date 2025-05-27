343c1eea7a5ba0fa4fb8d49d9f6b0861814ca541
Proxynich <162671763+Proxynich@users.noreply.github.com>
2025-05-27 15:21:37 +0700

    Added Shell for Binding NIC

---
+++ dpdk-bind.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Unbind NIC from Kernel Driver
+echo 0000:2d:00.0 > /sys/bus/pci/devices/0000:2d:00.0/driver/unbind
+echo 0000:2d:00.1 > /sys/bus/pci/devices/0000:2d:00.1/driver/unbind
+echo 0000:23:00.0 > /sys/bus/pci/devices/0000:23:00.0/driver/unbind
+echo 0000:23:00.1 > /sys/bus/pci/devices/0000:23:00.1/driver/unbind
+
+# Bind to DPDK Compatible Driver
+dpdk-devbind.py --bind=mlx5_core 0000:2d:00.0
+dpdk-devbind.py --bind=mlx5_core 0000:2d:00.1
+dpdk-devbind.py --bind=mlx5_core 0000:23:00.0
+dpdk-devbind.py --bind=mlx5_core 0000:23:00.1
