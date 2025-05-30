7bbb6009e84ab9e931f4cdfa0d4b7655371e54e7
Proxynich <162671763+Proxynich@users.noreply.github.com>
2025-05-30 11:13:26 +0700

    Update dpdk-bind.sh
    Added command to bring up the vfio driver

--- dpdk-bind.sh
+++ dpdk-bind.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Bring Up the Driver
+modprobe vfio-pci
+
 # Unbind NIC from Kernel Driver
 echo 0000:2d:00.0 > /sys/bus/pci/devices/0000:2d:00.0/driver/unbind
 echo 0000:2d:00.1 > /sys/bus/pci/devices/0000:2d:00.1/driver/unbind
@@ -9,5 +12,5 @@ echo 0000:23:00.1 > /sys/bus/pci/devices/0000:23:00.1/driver/unbind
 # Bind to DPDK Compatible Driver
 dpdk-devbind.py --bind=mlx5_core 0000:2d:00.0
 dpdk-devbind.py --bind=mlx5_core 0000:2d:00.1
-dpdk-devbind.py --bind=mlx5_core 0000:23:00.0
-dpdk-devbind.py --bind=mlx5_core 0000:23:00.1
+dpdk-devbind.py --bind=vfio-pci 0000:23:00.0
+dpdk-devbind.py --bind=vfio-pci 0000:23:00.1
